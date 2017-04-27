b173d7534cd9e0a667c1656a6223949e213bf619
Anoja Rajalakshmi <anoja@sfs-wifi-dhcp-10-143-51-198.wifi.sfsu.edu>
2017-04-27 13:52:11 -0700

    first commit

---
+++ esp8266_Photores.ino
@@ -0,0 +1,84 @@
+
+#include <ESP8266WiFi.h>
+#include <MQTTClient.h>
+
+
+int frequency=1000; //Specified in Hz
+int buzzPin= D2; 
+int timeOn=1000; //specified in milliseconds
+int timeOff=1000; //specified in millisecods
+/*const char* ssid = "ATT2.4";
+const char* password = "Srirama7*";*/
+const char* ssid = "aftab";
+const char* password = "abgoosht";
+const char* mqtt_server = "ec2-54-205-193-143.compute-1.amazonaws.com";
+
+WiFiClient net;
+MQTTClient client;
+unsigned long lastMillis = 0;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  client.begin(mqtt_server, net);
+  connect();
+  client.publish("/switch", "initialised");
+}
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("arduino", "try", "try")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+}
+
+
+void loop() {
+  if (!client.connected()) {
+    connect();
+  }
+        
+
+          char str[150];
+          String msg="Button status: ";
+          // read the input on analog pin 0:
+          int sensorValue = analogRead(A0);
+          // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+          float voltage = sensorValue * (5.0 / 1023.0);
+          // print out the value you read:
+          
+        //dtostrf(floatVar, minStringWidthIncDecimalPoint, numVarsAfterDecimal, charBuf);
+    
+          dtostrf(voltage, 0, 2, str);
+          Serial.println(voltage);
+          if (voltage<1.0)
+          {
+            client.publish("/photo","dark ");
+            tone(buzzPin, frequency);
+            delay(timeOn);
+            noTone(buzzPin);
+            delay(timeOff);
+
+          }
+          else
+          {
+            client.publish("/photo","day light sufficient ");
+          }
+           
+}
+
+void messageReceived(String topic, String payload, char * bytes, unsigned int length) {
+  Serial.print("incoming: ");
+  Serial.print(topic);
+  Serial.print(" - ");
+  Serial.print(payload);
+  Serial.println();
+}
